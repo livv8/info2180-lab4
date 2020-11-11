c0c78b98c02ad1cef5afa92a54e076c0df40e5e9
livv8 <66812129+livv8@users.noreply.github.com>
2020-11-10 23:39:42 -0500

    Exercise 1

---
+++ app.js
@@ -0,0 +1,11 @@
+var httpRequest = new XMLHttpRequest();
+var url = "http://localhost/info2180-lab4/superheroes.php";
+
+//Create Button
+var button=
+document.createElement("button");
+button.innerHTML = "Search";
+
+var button=
+document.createElement("button");
+button.innerHTML = "OK";
